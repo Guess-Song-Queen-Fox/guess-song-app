cfb7b66c742daf24928871779bed12f1109f2170
fauzanaldiansyah <fauzanaldiansyah@gmail.com>
2020-06-25 21:55:39 +0700

    Client layout added

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
     </div>
     <router-view/>
   </div>
@@ -15,6 +13,7 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: black;
 }
 
 #nav {
---
+++ client/src/views/Rooms.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <div class="jumbotron">
+      <h1 class="display-4">Choose Your Room:</h1>
+    </div>
+
+    <div class="choose1">
+      <div class="row text-center h-100">
+        <div class="col-sm d-flex flex-column bg-primary">
+          <div class="my-3">
+            <h1>Pop</h1>
+          </div>
+        </div>
+
+        <div class="col-sm d-flex flex-column bg-warning">
+          <div class="my-3">
+            <h1>Rock</h1>
+          </div>
+        </div>
+
+        <div class="col-sm d-flex flex-column bg-light">
+          <div class="my-3">
+            <h1>Jazz</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="choose2">
+      <div class="row text-center h-100">
+        <div class="col-sm d-flex flex-column bg-success">
+          <div class="my-3">
+            <h1>Indonesia</h1>
+          </div>
+        </div>
+
+        <div class="col-sm d-flex flex-column bg-danger">
+          <div class="my-3">
+            <h1>Rock</h1>
+          </div>
+        </div>
+
+        <div class="col-sm d-flex flex-column bg-info">
+          <div class="my-3">
+            <h1>Jazz</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <br />
+    <img alt="mic" src="../assets/mic.png" width="100" />
+    <img alt="mic" src="../assets/mic.png" width="100" />
+    <img alt="mic" src="../assets/mic.png" width="100" />
+    <img alt="mic" src="../assets/mic.png" width="100" />
+    <img alt="mic" src="../assets/mic.png" width="100" />
+    <img alt="mic" src="../assets/mic.png" width="100" />
+    <img alt="mic" src="../assets/mic.png" width="100" />
+    <img alt="mic" src="../assets/mic.png" width="100" />
+    <img alt="mic" src="../assets/mic.png" width="100" />
+    <img alt="mic" src="../assets/mic.png" width="100" />
+    <img alt="mic" src="../assets/mic.png" width="100" />
+    <img alt="mic" src="../assets/mic.png" width="100" />
+    <img alt="mic" src="../assets/mic.png" width="100" />
+    <img alt="mic" src="../assets/mic.png" width="100" />
+    <br />
+    <br />
+    <br />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
+// import socket from "@/config/socket";
+
+export default {
+  name: "Home",
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      username: ""
+    };
+  },
+  methods: {
+    getUsername() {
+      this.$router.push("./rooms");
+      socket.emit("send-username", this.username);
+    }
+  },
+  created() {
+    socket.on("new-player", response => {
+      console.log("listening dari server");
+    });
+  }
+};
+</script>
